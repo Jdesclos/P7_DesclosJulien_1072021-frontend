3b1bab0b4227c7198bf699afcff1d59c3c0d8f83
Shadow <desclos.julien@gmail.com>
2022-01-21 10:20:51 +0100

    cors fix

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import { faClock } from '@fortawesome/free-regular-svg-icons'
 import { faThumbsUp  as farThumbsUp} from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = process.env.BASE_URL /*|| 'http://localhost:8080'*/;
+axios.defaults.baseURL = "https://groupomaniadesclos.herokuapp.com/"/*|| 'http://localhost:8080'*/;
 console.log(axios.defaults)
 axios.interceptors.response.use(undefined, function (error) {
   if (error) {
